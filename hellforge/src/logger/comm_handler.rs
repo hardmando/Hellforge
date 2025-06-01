3be236d7371600940690d3e0e9bbe3c5c9128be9
hardmando <totonn777@icloud.com>
2025-05-31 20:03:43 -0500

    Files sent to server

--- hellforge/src/logger/comm_handler.rs
+++ hellforge/src/logger/comm_handler.rs
@@ -1,5 +1,7 @@
 use reqwest::blocking::Client;
+use reqwest::blocking::multipart;
 use serde::Serialize;
+use std::fs::File;
 
 #[derive(Serialize)]
 pub struct SyncEvent {
@@ -10,9 +12,15 @@ pub struct SyncEvent {
 
 pub fn send_event(event: &SyncEvent) -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
+
+    let file = File::open(&event.path)?;
+    let form = multipart::Form::new()
+        .text("event", serde_json::to_string(event)?)
+        .file("file", &event.path)?;
+
     let res = client
         .post("http://localhost:8080/event")
-        .json(&event)
+        .multipart(form)
         .send()?;
 
     if res.status().is_success() {
--- server/main.go
+++ server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 type SyncEvent struct {
@@ -14,14 +16,41 @@ type SyncEvent struct {
 }
 
 func handleEvent(w http.ResponseWriter, r *http.Request) {
-	var event SyncEvent
-	err := json.NewDecoder(r.Body).Decode(&event)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
+	r.ParseMultipartForm(10 << 20)
 
+	eventJson := r.FormValue("event")
+	var event SyncEvent
+
+	// err := json.NewDecoder(r.Body).Decode(&event)
+	// if err != nil {
+	//	http.Error(w, "Invalid JSON", http.StatusBadRequest)
+	//	return
+
+	//}
+
+	if err := json.Unmarshal([]byte(eventJson), &event); err != nil {
+		http.Error(w, "Invalid event JSON", http.StatusBadRequest)
+		return
 	}
-	log.Printf("Received Event: %+v\n", event)
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "File Not Found", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	dstPath := "storage/uploads" + handler.Filename
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		http.Error(w, "Could not save file", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	io.Copy(dst, file)
+
+	log.Printf("Received Event for %s and saved at %s", event.Path, dst)
 	w.WriteHeader(http.StatusOK)
 }
 
